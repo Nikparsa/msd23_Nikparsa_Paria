abaa7e6c1326f26b48a76588966a6caefcf208d2
Paria Nikparsa <paria.nikparsa@edu.fh-joanneum.at>
2024-05-29 19:02:10 +0200

    Implemented factorial method, added and passed all tests

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -21,7 +21,12 @@ public class Calculator {
     }
 
     public int factorial(int n) {
-
-        return 0;
+        if(n < 0)
+            return 0;
+        else if (n == 0 || n == 1) {
+            return 1;
+        } else
+            return n * factorial(n - 1);
     }
+
 }

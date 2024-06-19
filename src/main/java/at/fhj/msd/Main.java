34aa3c592eb474e62314738324c395c79a41caee
Paria Nikparsa <paria.nikparsa@edu.fh-joanneum.at>
2024-06-19 20:25:57 +0200

    Save changes before switching branch

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,35 +1,33 @@
 package at.fhj.msd;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static Logger logger = LogManager.getLogger(Main.class);
 
-    private static Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
-
-        logger.error("It is an error logger.");
-
-        logger.info("It is a info logger.");
-
-
-
-
+        logger.info("Starting application");
 
         Calculator calculator = new Calculator();
 
+        try {
+            double resultAddition = calculator.add(3, 5);
+            System.out.println("Result of addition: " + resultAddition);
 
-        double resultAddition = calculator.add(3, 5);
-        System.out.println("Result of addition: " + resultAddition);
+            double resultSubtraction = calculator.subtract(18, 12);
+            System.out.println("Result of subtraction: " + resultSubtraction);
 
-        double resultSubtraction = calculator.subtract(18, 12);
-        System.out.println("Result of subtraction: " + resultSubtraction);
+            double resultMultiplication = calculator.multiply(25, 57);
+            System.out.println("Result of multiplication: " + resultMultiplication);
 
-        double resultMultiplication = calculator.multiply(25, 57);
-        System.out.println("Result of multiplication: " + resultMultiplication);
+            double resultDivision = calculator.divide(456, 12);
+            System.out.println("Result of division: " + resultDivision);
+        } catch (ArithmeticException e) {
+            logger.error("Exception occurred: ", e);
+        }
 
-        double resultDivision = calculator.divide(456, 12);
-        System.out.println("Result of division: " + resultDivision);
-        String myName = "Paria Nikparsa";
-        System.out.println(myName);
+        logger.info("Application finished");
     }
 }
